2cd1455c2513075114755f34e759f4121b87b8e4
MrMobbi <julliatmathias06@gmail.com>
2023-10-04 15:22:22 +0200

    WIP

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 /etc/init.d/mariadb start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRAND ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-/usr/bin/mysqld_safe
+echo "CREATE DATABASE IF NOT EXISTS $SQL_NAME ;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD' ;" >> db.sql
+echo "GRANT ALL PRIVILEGES ON $SQL_NAME.* TO '$SQL_USER'@'%' ;" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234' ;" >> db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
+
+/usr/bin/mysql < db.sql
+
+mysqladmin -u root -p$SQL_ROOT_PWD shutdown
+
+exec "$@"
--- srcs/requirements/wordpress/tools/config_wp.sh
+++ srcs/requirements/wordpress/tools/config_wp.sh
@@ -2,17 +2,48 @@
 
 sleep 10
 
-if [ ! -e ./wp-config.php ]
+if [ ! -e ./var/www/html/wp-config.php ]
 then
 
     # installation of CLI [inteface textuel pour ligne de commande]
     wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
-    && chmod +x wp-cli.phar
+         && chmod +x wp-cli.phar
 
     # installing wordpress
     ./wp-cli.phar core download http://wordpress.org/latest.tar.gz \
-    --allow-root \
-    --path='./'
+                                --allow-root \
+                                --path='/var/www/html'
+    
+  ./wp-cli.phar config create --allow-root \
+                              --dbname=${SQL_NAME} \
+                              --dbuser=${SQL_USER} \
+                              --dbpass=${SQL_PWD} \
+                              --dbhost=${SQL_HOST} \
+                              --dbcharset="utf8" \
+                              --dbcollate="utf8_general_ci" \
+                              --path='/var/www/html'
+  
+  sleep 10
+
+  # Second page
+  ./wp-cli.phar core install --allow-root \
+                             --url=${WP_SITE_URL} \
+                             --title=${WP_SITE_TITLE} \
+                             --admin_user=${WP_ADMIN_USER} \
+                             --admin_password=${WP_ADMIN_PWD} \
+                             --admin_email=${WP_ADMIN_EMAIL} \
+                             --skip-email \
+                             --path='/var/www/html'
+
+  sleep 10
+
+  # Create user
+  ./wp-cli.phar user create --allow-root \
+                            --role=author ${WP_USER} ${WP_USER_EMAIL} \
+                            --user_pass=${WP_USER_PWD} \
+                            --path='/var/www/html'
+
+    chmod 775 /var/www/html/*
 fi
 
 exec "$@"
