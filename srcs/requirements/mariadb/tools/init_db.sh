ceeab8dac872f6937532b368a6e0ed005ecbd5ad
Mathias Julliat <mjulliat@c1r3s5.42lausanne.ch>
2023-10-10 13:12:59 +0200

    WIP

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-/etc/init.d/mariadb start
+service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $SQL_NAME ;" > db.sql
-echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD' ;" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $SQL_NAME.* TO '$SQL_USER'@'%' ;" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234' ;" >> db.sql
+echo "CREATE DATABASE IF NOT EXISTS $SQL_NAME;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD';" >> db.sql
+echo "GRANT ALL PRIVILEGES ON $SQL_NAME.* TO '$SQL_USER'@'%';" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
-/usr/bin/mysql < db.sql
+mariadb < db.sql
 
-mysqladmin -u root -p$SQL_ROOT_PWD shutdown
+echo "coucou"
+
+service mariadb stop
+
+echo "coucou 2"
 
 exec "$@"
--- srcs/requirements/wordpress/tools/config_wp.sh
+++ srcs/requirements/wordpress/tools/config_wp.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
-while ! mariadb -h $SQL_HOST -u $SQL_USER -p $SQL_PWD $SQL_NAME &>/dev/null; do
-  sleep 3
-done
+sleep 10
 
-if [ ! -e ./var/www/html/wp-config.php ]
-then
+if [ ! -f /var/www/html/wp-config.php ]; then
 
-    # installation of CLI [inteface textuel pour ligne de commande]
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
+  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+  chmod 777 wp-cli.phar && \
+  #me executable dans /bin car PATH dans ENV
+  mv wp-cli.phar /usr/local/bin/wp && \
+  cd /var/www/html && \
 
-    # installing wordpress
-    ./wp-cli.phar core download http://wordpress.org/latest.tar.gz \
-                                --allow-root \
-                                --path='/var/www/html'
-    
-  ./wp-cli.phar config create --allow-root \
-                              --dbname=${SQL_NAME} \
-                              --dbuser=${SQL_USER} \
-                              --dbpass=${SQL_PWD} \
-                              --dbhost=${SQL_HOST} \
-                              --dbcharset="utf8" \
-                              --dbcollate="utf8_general_ci" \
-                              --path='/var/www/html'
-  
-  sleep 10
+   # installing wordpress
+  wp core download http://wordpress.org/latest.tar.gz \
+                   --allow-root \
+                   --path='/var/www/html'
+
+  echo "coucou"
+
+  wp config create --allow-root \
+                   --dbname=$SQL_NAME \
+                   --dbuser=$SQL_USER \
+                   --dbpass=$SQL_PWD \
+                   --dbhost="mariadb" \
+                   --dbcharset="utf8" \
+                   --dbcollate="utf8_general_ci" \
+                   --path='/var/www/html'
+
 
   # Second page
-  ./wp-cli.phar core install --allow-root \
-                             --url=${WP_SITE_URL} \
-                             --title=${WP_SITE_TITLE} \
-                             --admin_user=${WP_ADMIN_USER} \
-                             --admin_password=${WP_ADMIN_PWD} \
-                             --admin_email=${WP_ADMIN_EMAIL} \
-                             --skip-email \
-                             --path='/var/www/html'
+  wp core install --allow-root \
+                  --url=$WP_SITE_URL \
+                  --title=$WP_SITE_TITLE \
+                  --admin_user=$WP_ADMIN_USER \
+                  --admin_password=$WP_ADMIN_PWD \
+                  --admin_email=$WP_ADMIN_EMAIL \
+                  --skip-email \
+                  --path='/var/www/html'
 
-  sleep 10
 
   # Create user
-  ./wp-cli.phar user create --allow-root \
-                            --role=author ${WP_USER} ${WP_USER_EMAIL} \
-                            --user_pass=${WP_USER_PWD} \
-                            --path='/var/www/html'
+  wp user create --allow-root \
+                 --role=author $WP_USER $WP_USER_EMAIL \
+                 --user_pass=$WP_USER_PWD \
+                 --path='/var/www/html'
 
-    chmod 775 /var/www/html/*
+  chmod 775 /var/www/html/*
+
 fi
 
 exec "$@"
